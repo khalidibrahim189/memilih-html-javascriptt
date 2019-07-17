6908330f3fb0761a64742ba4ee028c65c53f9eab
khalidjambi <khalid.jambi18@gmail.com>
2019-07-17 07:15:53 +0700

    kedua

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById('box1').innerHTML = "Sekolah koding";
+
+var boxes = document.getElementsByClassName('boxes');
+boxes[0].innerHTML = "versi terbaru";
+boxes[1].innerHTML = "versi sebelumnya";
+boxes[2].innerHTML = "versi terlama";
+
+var list = document.getElementsByTagName('li');
+li[0].innerHTML = "SASS";
+li[1].innerHTML = "PHP";
+li[2].innerHTML = "JQUERY";
